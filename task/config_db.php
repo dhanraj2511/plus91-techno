942b36d35985f4b8658bf7b6a2f75e91e20b0291
dhanraj2511 <53075470+dhanraj2511@users.noreply.github.com>
2021-12-27 17:41:18 +0530

    Update with Doctrine DBAL Quires

--- task/config_db.php
+++ task/config_db.php
@@ -1,5 +1,16 @@
 <?php
-$connection = new mysqli("localhost","root","","patient_db");
+/*$connection = new mysqli("localhost","root","","patient_db");
 if (! $connection){
     die("Error in connection".$connection->connect_error);
-}
+}*/
+require_once "vendor/autoload.php";
+
+use Doctrine\DBAL\DriverManager;
+$connection = array(
+    'dbname' => 'patient_db',
+    'user' => 'root',
+    'password' => '',
+    'host' => 'localhost',
+    'driver' => 'pdo_mysql',
+);
+$conn = \Doctrine\DBAL\DriverManager::getConnection($connection);
